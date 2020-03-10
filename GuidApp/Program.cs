23c83e627042c9cc40466b39e5f0760311b2ff5e
Ritzy-Rosabel <mipoolugbenga@gmail.com>
2020-03-10 14:32:59 +0100

    Add project files.

---
+++ GuidApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuidApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+         for (int i = 0; i<=10; i++)
+            {
+            Guid obj = Guid.NewGuid();
+            Console.WriteLine("New Guid is " + obj.ToString());
+            }
+            Console.ReadLine();
+           return -1;
+        }
+    }
+}
